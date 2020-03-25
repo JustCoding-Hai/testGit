bf80e2c1e5041c4046b334f0ed63a1a6b67184d2
Eclipse_User <Eclipse_user@aliyun.com>
2020-03-25 21:17:04 +0800

    Edit by hot_fix

--- src/main/java/com/learn/git/Banana.java
+++ src/main/java/com/learn/git/Banana.java
@@ -2,7 +2,8 @@ package com.learn.git;
 
 public class Banana {
 	public static void main(String[] args) {
-		System.out.println("left");
+		System.out.println("this is right");
+		System.out.println("Edit By branch hot_fix");
 	}
 
 }
